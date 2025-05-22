72ac0544dbeb924b20c77cb3f166140b01d00834
ankurraj007 <rajguptaankur@gmail.com>
2025-05-22 11:49:05 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: '/cleanx_v1/',
+  base: '/cleanx/',
   plugins: [react()],
 })
